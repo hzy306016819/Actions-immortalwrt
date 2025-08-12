07ef1a730a99d75915f09ac01842b2d76e7099d9
hzy306016819 <306016819@qq.com>
2025-08-12 20:22:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,9 +17,16 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# 添加 luci-app-overview-widgets 插件
-#echo 'src-git overview_widgets https://github.com/hzy306016819/luci-app-overview-widgets.git' >> feeds.conf.default
-#git clone https://github.com/hzy306016819/luci-app-overview-widgets.git package/overview-widgets
+# 移除冲突的包golang-v2ray-geodata
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+
+# 添加高版本 golang 24.x（mosdns 依赖）
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+# 添加 mosdns 和 v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # Add package
 # 增加ssid-auto到package/custom
