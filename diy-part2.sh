4bbf9d5cd77945817ad6e7c746f027c2e4d1719b
hzy306016819 <306016819@qq.com>
2025-08-14 15:23:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 # diy-part2.sh - 整合mosdns和overview-widgets的添加逻辑
 
-# 在.config中启用所有需要的包
-echo "启用必要组件..."
-echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> .config
-echo "CONFIG_PACKAGE_mosdns=y" >> .config
-echo "CONFIG_PACKAGE_v2ray-geodata=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-overview-widgets=y" >> .config  # 启用overview-widgets
-# 验证配置是否生效
-grep "CONFIG_PACKAGE_luci-app-mosdns" .config || echo "错误：mosdns 未启用！"
-grep "CONFIG_PACKAGE_luci-app-overview-widgets" .config || echo "错误：luci-app-overview-widgets 未启用！"
-
 # Modify default IP
 sed -i 's/192.168.6.1/192.168.100.10/g' package/base-files/files/bin/config_generate
 
